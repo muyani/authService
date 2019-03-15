4d74ed333d378a0260b7020f96703efa2db7e853
muyani <muyaniletina2@gmail.com>
2019-03-15 17:07:37 +0300

    stashed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
+
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sentry_sdk
@@ -14,17 +14,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some-secret-string'
 db = SQLAlchemy(app)
 
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-jwt = JWTManager(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-@jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return models.RevokedTokenModel.is_jti_blacklisted(jti)
@@ -32,15 +26,15 @@ def check_if_token_in_blacklist(decrypted_token):
 api = Api(app)
 
 import models, resources
+
 api.add_resource(resources.UserRegistration, '/register')
 api.add_resource(resources.Homepage, '/')
 api.add_resource(resources.UserLogin, '/login')
-api.add_resource(resources.UserLogoutAccess, '/logout/access')
-api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
-api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.UserLogout, '/logout')
+# api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+# api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
-
 
 # if __name__ == "__main__":
 #     app.run(debug=True,port=5000)
--- resources.py
+++ resources.py
@@ -1,9 +1,6 @@
 from flask import request
 from flask_restful import Resource
 from models import UserModel,RevokedTokenModel
-from flask_jwt_extended import (create_access_token, create_refresh_token,
-                                jwt_required, jwt_refresh_token_required,
-                                get_jwt_identity, get_raw_jwt)
 from sentry_sdk import capture_exception,capture_message
 
 
@@ -26,15 +23,11 @@ class UserRegistration(Resource):
         try:
             user = UserModel(fullName=fullName, email=email, password=UserModel.generate_hash(password))
             user.save_to_db()
-            access_token = create_access_token(identity=email)
-            refresh_token = create_refresh_token(identity=email)
-            return {'message': 'User {} was created'.format(email),
-                    'access_token': access_token,
-                    'refresh_token': refresh_token
-                 },200
+            return {'message':email},201
+
         except Exception as e:
             capture_exception(e)
-            return {"Message":"Unable to create user"},500
+            return {"message":"Unable to create user"},500
 
 
 
@@ -42,68 +35,49 @@ class UserLogin(Resource):
     def post(self):
         if request.is_json:
             try:
-                username = request.get_json()['username']
+                email = request.get_json()['email']
                 password = request.get_json()['password']
             except KeyError:
-                return {"Message": "Some field is empty"}, 400
+                return {"message": "Some field is empty"}, 400
         else:
-            return {"Message": "Request body is null/not JSON"}, 400
-        current_user = UserModel.find_by_username(username)
+            return {"message": "Request body is null/not JSON"}, 400
+        current_user = UserModel.find_by_email(email)
         if not current_user:
-            return {"Message":"User {} doesnt exist".format(username)},400
+            return {"message":"User {} doesnt exist".format(email)},400
         if UserModel.verify_hash(password,current_user.password):
-            access_token = create_access_token(identity=username)
-            refresh_token = create_refresh_token(identity=username)
             return {
-            'message': 'User {} Logged in Successfully'.format(username),
-            'access_token': access_token,
-            'refresh_token': refresh_token
+            'message': email
                  },200
         else:
             return {'message': 'Wrong credentials'},403
 
-class UserLogoutAccess(Resource):
-    @jwt_required
+class UserLogout(Resource):
     def post(self):
-        print(get_raw_jwt())
-        jti = get_raw_jwt()['jti']
         try:
-            revoked_token = RevokedTokenModel(jti = jti)
-            revoked_token.add()
             return {'message': 'Access token has been revoked'},200
         except:
             return {'message': 'Something went wrong'}, 500
 
 
 class UserLogoutRefresh(Resource):
-    @jwt_refresh_token_required
     def post(self):
-        jti = get_raw_jwt()['jti']
         try:
-            revoked_token = RevokedTokenModel(jti = jti)
-            revoked_token.add()
             return {'message': 'Refresh token has been revoked'}
         except:
             return {'message': 'Something went wrong'}, 500
 
 class TokenRefresh(Resource):
-    @jwt_refresh_token_required
     def post(self):
-        current_user = get_jwt_identity()
-        print(current_user)
-        access_token = create_access_token(identity = current_user)
-        return {'access_token': access_token},200
+        return {'access_token'},200
 
 class AllUsers(Resource):
-    @jwt_required
     def get(self):
         return UserModel.return_all()
-    @jwt_required
-    def delete(self):
-        return UserModel.delete_all()
+
+    # def delete(self):
+    #     return UserModel.delete_all()
 
 class SecretResource(Resource):
-    @jwt_required
     def get(self):
         return {
             'answer': 42
@@ -111,4 +85,4 @@ class SecretResource(Resource):
 
 class Homepage(Resource):
     def get(self):
-        return {'Message':'This is a private api'},200
+        return {'message':'This is a private api'},200
